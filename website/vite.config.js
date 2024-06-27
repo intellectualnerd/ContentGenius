40ed9e416ffd6c36a367dbaf2496cfd7ce3ff9d2
JAINAM576 <het170246@gmail.com>
2024-06-27 18:08:34 +0530

    FONTS PRELOADER IMPLEMENTED

--- website/vite.config.js
+++ website/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allow access from the local network
-    port: 5173 // Specify a port
-  }
+  
 })
