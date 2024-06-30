a09b2fae6c9b22c7c7a003fd00195acdb32a7ee6
unknown <parmarparth954@gmail.com>
2024-07-01 02:09:52 +0530

    css + added carousel

--- website/src/Pages/Info/components/Reviews.jsx
+++ website/src/Pages/Info/components/Reviews.jsx
@@ -10,12 +10,12 @@ const Reviews = ({ reviews }) => {
                     <Carousel.Item key={index}>
                         <div className="d-flex justify-content-center w-100 h-100">
                             <div className="review-box row ">
-                                <div className='col-lg-2 col-md-2 col-sm-12' >
+                                <div className='col-lg-2 col-md-2 col-sm-12'>
                                     <div className="user-photo">
                                         <img src={review.photo} alt={review.name} />
                                     </div>
                                 </div>
-                                <div className='col-lg-9 col-md-9 col-sm-12 gy-0 carousel-review-text'>
+                                <div className='col-lg-10 col-md-10 col-sm-12 gy-0 carousel-review-text'>
                                     <span>{review.name}</span>
                                     <span>{review.email}</span>
                                 </div>
