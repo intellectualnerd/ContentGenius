26d040717ecb6ba9ef887f8d15f3396546d7b6ad
unknown <parmarparth954@gmail.com>
2024-07-01 02:32:00 +0530

    css + added carousel

--- website/src/Pages/Info/components/Reviews.jsx
+++ website/src/Pages/Info/components/Reviews.jsx
@@ -8,8 +8,9 @@ const Reviews = ({ reviews }) => {
                 {reviews.map((review, index) => (
 
                     <Carousel.Item key={index}>
+                        <div className='fix-carousel-height'>
                         <div className="d-flex justify-content-center w-100 h-100">
-                            <div className="review-box row ">
+                            <div className="review-box row">
                                 <div className='col-lg-2 col-md-2 col-sm-12'>
                                     <div className="user-photo">
                                         <img src={review.photo} alt={review.name} />
@@ -20,11 +21,13 @@ const Reviews = ({ reviews }) => {
                                     <span>{review.email}</span>
                                 </div>
 
-                                <div className="review-content col-12 text-start mt-4">
+                                <div className="review-content col-12 text-start mt-2">
                                     <span>{review.description}</span>
                                 </div>
                             </div>
                         </div>
+                        </div>
+
 
                     </Carousel.Item>
                 ))}
